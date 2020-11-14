384561f95f4705414c43f0a13de52450b434c83f
Anton Boyd <antonjboyd@gmail.com>
2020-11-14 10:56:48 +0000

    Scrapes all equipment along with price sold by every dealer. Added pagination.

--- farm_machinary_dealerships/spiders/dealership_scrape.py
+++ farm_machinary_dealerships/spiders/dealership_scrape.py
@@ -15,7 +15,7 @@ class DealershipScrapeSpider(scrapy.Spider):
 
 
     def parse_stores(self,response):
-        stores = response.css('.col-xs-6 > ul > li > a::attr(href)').getall()
+        stores = response.css('.col-xs-6 a::attr(href)').getall()
         for store in stores:
             store_page = response.urljoin(store)
             yield scrapy.Request(url=store_page, callback=self.parse_data)
@@ -23,11 +23,34 @@ class DealershipScrapeSpider(scrapy.Spider):
 
 
     def parse_data(self, response):
-        details = response.css('.col-sm-3.col-xs-12')
-        for detail in details:
-            header = detail.css('.store-header::text').get()
-            item = detail.css('.store-item::text').getall()
+        company_name = response.css('h1::text').get()
+        products = response.css('.dc-store-wrapper')
+        details = response.css('.store-header:contains("Contact") ~ div::text').getall()
+        details = [x for x in details if x!='\n'] #remove \n
+        detail = ','.join(details) #list to string
+
+        address_list = response.css('.store-header:contains("Address") ~ div::text').getall()
+        address_list = [x for x in address_list if x != '\n']  # remove \n
+        address = ', '.join(address_list)  # list to string with comma
+
+        yield {
+            'dealer_name': company_name,
+            'contact': detail,
+            'address': address,
+        }
+
+        for product in products:
+            listing_name = product.css('.listing-name > a::text').get()
+            listing_price = product.css('.listing-price::text').get()
 
             yield {
-                header: item,
+                'listing_name': listing_name,
+                'listing_price': listing_price,
             }
+
+            next_page_url = response.css('#panel li:nth-child(6) a::attr(href)').get()
+            if next_page_url:
+                next_page_url = response.urljoin(next_page_url)
+                yield scrapy.Request(url=next_page_url, callback=self.parse_data)
+
+
